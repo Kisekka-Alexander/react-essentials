5413aaafbdb0f20494c280441f9830e93eabba18
Kisekka-Alexander <53447358+Kisekka-Alexander@users.noreply.github.com>
2022-02-22 12:07:55 +0300

    understanding the usestate

--- src/App.js
+++ src/App.js
@@ -1,55 +1,63 @@
-
 import './App.css';
-import gitdp from './gitdp.jpg';
+import React, {useState} from "react";
 
-function Header(props){
-  return(
-    <header>
-      <h1> Welcome to Maama {props.name}'s Restaurant</h1>
-    </header>
-  )
-}
-
-function Main(props){
-  return(
-    <section>
-      <p> We serve the most {props.adjective} food around.</p>
-      <img src="https://github.com/Kisekka-Alexander.png" height={200}/>
-      <ul style={{textAlign: "left"}}>
-        {props.dishes.map((dish) => 
-        (<li key={dish.id}>{dish.title}</li>))}
-      </ul>
-    </section>
-  )
-}
-
-function Footer(props){
-  return(
-    <footer>
-      <p> Copyright {props.year} </p>
-    </footer>
-  )
-}
-
-const dishes = [
-  "Matooke + Fish",
-  "Rice + chicken",
-  "Gnuts + Matooke",
-  "Meat + Sweetpotatoes"
-
-];
-
-const dishObjects = dishes.map((dish,i) => ({id:i, title: dish }));
-console.log(dishObjects);
 function App() {
-  return (
-    <>
-
-      <Header name="Shirat"/>
-      <Main adjective="amazing" dishes={dishObjects}/>
-      <Footer year = {new Date().getFullYear()}/>
-    </>
-  );
+const [emotion , setEmotion] = useState("happy");
+return (
+  <>
+  <h1> Current emotion is {emotion}.</h1>
+  <button onClick={() => setEmotion("frustrated")}>Frustrated</button>
+  </>
+);
 }
 
 export default App;
+
+
+
+
+
+
+
+
+// import gitdp from './gitdp.jpg';
+
+// function Header(props){
+//   return(
+//     <header>
+//       <h1> Welcome to Maama {props.name}'s Restaurant</h1>
+//     </header>
+//   )
+// }
+
+// function Main(props){
+//   return(
+//     <section>
+//       <p> We serve the most {props.adjective} food around.</p>
+//       <img src="https://github.com/Kisekka-Alexander.png" height={200}/>
+//       <ul style={{textAlign: "left"}}>
+//         {props.dishes.map((dish) => 
+//         (<li key={dish.id}>{dish.title}</li>))}
+//       </ul>
+//     </section>
+//   )
+// }
+
+// function Footer(props){
+//   return(
+//     <footer>
+//       <p> Copyright {props.year} </p>
+//     </footer>
+//   )
+// }
+
+// const dishes = [
+//   "Matooke + Fish",
+//   "Rice + chicken",
+//   "Gnuts + Matooke",
+//   "Meat + Sweetpotatoes"
+
+// ];
+
+// const dishObjects = dishes.map((dish,i) => ({id:i, title: dish }));
+// console.log(dishObjects);
